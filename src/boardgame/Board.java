bc4d3e7cc5616fe2b944a16dcdbf7c67cce33c4f
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2021-02-02 14:31:14 -0400

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -45,6 +45,19 @@ public class Board {
 		piece.position = position;
 	}
 
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board!");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	private boolean positionExists(int row, int column) {
 		return (0 <= row && row < rows) && (0 <= column && column < columns);
 	}
