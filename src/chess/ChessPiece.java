968910a3bb2fa38fd42c740d2e98fa875281f0c0
Jeferson Willian Vieira Silva <57540297+jefersonwvs@users.noreply.github.com>
2021-02-05 09:22:22 -0400

    Check logic - part 1

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,6 +17,10 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece opponentPiece = (ChessPiece)getBoard().piece(position);
 		return opponentPiece != null && opponentPiece.getColor() != color;
